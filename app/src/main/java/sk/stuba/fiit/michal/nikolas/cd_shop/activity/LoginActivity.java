49e17a13a370fea9a14e5ae65c9e26e0c5ef7f55
mintos5 <michal.skuta@gmail.com>
2016-03-25 16:11:16 +0100

    Added login screen

---
+++ app/src/main/java/sk/stuba/fiit/michal/nikolas/cd_shop/activity/LoginActivity.java
@@ -0,0 +1,25 @@
+package sk.stuba.fiit.michal.nikolas.cd_shop.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import sk.stuba.fiit.michal.nikolas.cd_shop.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar.setTitle(R.string.action_login);
+
+        setSupportActionBar(mToolbar);
+        //getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+}
